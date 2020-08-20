cc791236f1c3727edf1811e9051ba1049db2fa67
Przemyslaw Pajak <przemyslaw.pajak@creative-works.us>
2020-08-20 23:45:17 +0200

    cirq

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import cirq
 
 circuit = cirq.Circuit()
+(q0, q1) = cirq.LineQubit.range(2)
+
+# Add gates
+circuit.append([cirq.H(q0), cirq.CNOT(q0, q1)])
+# Measure quantum bits
+circuit.append([cirq.measure(q0), cirq.measure(q1)])
+
+print(circuit)
+
+# Simulator
+sim = cirq.Simulator()
+results = sim.run(circuit, repetitions=10)
+print(results)
